3258f2b7db3f051cce91466fcadaf18a23e4e077
ksachin7 <isachin.imp@gmail.com>
2024-05-14 15:57:38 +0530

    added demosb project

---
+++ demosb/src/main/java/com/example/demo/DemosbApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosbApplication.class, args);
+	}
+
+}
